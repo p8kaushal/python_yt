3b6c3a4eb7b8328318d17ca0ecf9fe99cc9b6410
Parth Kaushal <parthkaushal1008@gmail.com>
2025-09-28 13:19:59 +0530

    storing video data in supabsae

--- get_vid_url.py
+++ get_vid_url.py
@@ -1,5 +1,7 @@
 import scrapetube
 import argparse
+import asyncio
+from prisma import Prisma
 
 from dotenv import load_dotenv
 import os
@@ -7,18 +9,9 @@ import os
 # Load environment variables from .env file
 load_dotenv()
 
-# Access the variables
-database_url = os.getenv('SUPABASE_URL')
-api_secret = os.getenv('SUPABASE_ANON_KEY')
-
-print(f"Database URL: {database_url}")
-print(f"API Secret: {api_secret}")
-
-
-from supabase import create_client, Client
-
-supabase: Client = create_client(database_url, api_secret)
-
+# # Access the variables
+# database_url = os.getenv('SUPABASE_URL')
+# api_secret = os.getenv('SUPABASE_ANON_KEY')
 
 parser = argparse.ArgumentParser(description="A script that processes command-line arguments.")
 parser.add_argument("-c", "--command", type=str, help="channel||playlist||search", default="channel")
@@ -42,6 +35,35 @@ match args.command.strip().lower():
     case _: # Wildcard pattern, acts as a default case
         print("Unknown command.")
 
+async def store() -> None:
+    prisma = Prisma()
+    await prisma.connect()
 
-for video in videos:
-    print("https://www.youtube.com/watch?v="+str(video['videoId']))
+    contentGroup = await prisma.contentgroup.create(
+        data={
+            'ref': args.value,
+            'type': 'Channel' if args.command.strip().lower() == 'channel' else 'Playlist' if args.command.strip().lower() == 'playlist' else 'Search',
+            'source': 'Youtube',
+        },
+    )
+
+    for video in videos:
+        print("https://www.youtube.com/watch?v="+str(video['videoId']))
+        # write your queries here
+        content = await prisma.content.create(
+            data={
+                'grpId': contentGroup.id,
+                'title': video['title']['runs'][0]['text'] if 'runs' in video['title'] else video['title']['simpleText'],
+                'description': '',
+                'url': "https://www.youtube.com/watch?v="+str(video['videoId']),
+                'thumbnail': '',
+                'viewCount': 0,
+                'length': 0,
+                'type': 'video',
+            },
+        )
+
+    await prisma.disconnect()
+
+if __name__ == '__main__':
+    asyncio.run(store())
